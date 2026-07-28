431c8bb64f5473c4176fc6a76ce5eba02fe76d8d
Chia Voltaire <chiavoltaire07>
2026-07-28 15:20:02 +0100

    added some missing files

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./features/home/pages/HomePage";
+import { SearchPage } from "./features/search";
+import About from "./pages/about/About";
+import ContactPage from "./pages/contact/ContactPage";
+import SchoolDetailsPage from "./pages/school-details/SchoolDetailsPage";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/search" element={<SearchPage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/school/:id?" element={<SchoolDetailsPage />} />
+    </Routes>
+  );
+}
